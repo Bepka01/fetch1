b9a31c5ed777d0de1c41a2266206b1469b21e2ce
unknown <nichegolichnogo1717@gmail.com>
2025-10-21 19:21:44 +0300

    addCheckDouble

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ const id = document.querySelector(".id");
 const title = document.querySelector(".title");
 const body = document.querySelector(".body");
 const loader = document.querySelector(".loader");
+
+const allLastID = [];
+
+const uniqeID = new Set();
+
+function getLastPost() {
+  const arrayFromSet = Array.from(uniqeID);
+  return arrayFromSet[arrayFromSet.length - 1];
+}
+
 function getRandomNumber() {
   return Math.floor(Math.random() * 100) + 1;
 }
@@ -14,29 +24,49 @@ function startLoader() {
   btn.disabled = true;
   btn.style.backgroundColor = "gray";
 }
+
 function endLoader() {
   loader.style.display = "none";
   btn.style.backgroundColor = "chocolate";
   btn.disabled = false;
 }
+async function getUniquePost(attempt = 1) {
+  if (attempt > 10) {
+    throw new Error("Не удалось найти уникальный пост после 10 попыток");
+  }
+  const randomId = getRandomNumber();
+  const url = `https://jsonplaceholder.typicode.com/posts/${randomId}`;
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  const post = await response.json();
+  if (uniqeID.has(post.id)) {
+    console.log("Этот пост уже был , грузим другой");
+    return getUniquePost(attempt + 1);
+  }
+  console.log("Этого поста не было");
+  return post;
+}
 async function getPosts() {
   try {
     startLoader();
-    const url = `https://jsonplaceholder.typicode.com/posts/${getRandomNumber()}`;
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-    const post = await response.json();
+    const post = await getUniquePost();
+
     userID.innerHTML = `id Полььзователя: ${post.userId}`;
     id.innerHTML = `id: ${post.id}`;
     title.innerHTML = `Title: ${post.title}`;
     body.innerHTML = `body: ${post.body}`;
+    allLastID.push(post.id);
+    uniqeID.add(post.id);
   } catch (error) {
     alert("ошибка запроса");
   } finally {
     console.log("Запрос совершен");
+    console.log(uniqeID);
+    console.log(getLastPost());
     endLoader();
   }
 }
+
 btn.addEventListener("click", getPosts);
